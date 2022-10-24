89d204b4843899c8a8e48ab4fdd8dd0a8f6245b9
Daniel Marcelino de Lima <varnahal0712@gmail.com>
2022-10-24 15:40:44 -0300

    importação de imagens

--- App001/app001/src/App.js
+++ App001/app001/src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-
+import Logo from './componentes/imgs/16002760_574313126111301_7408436797266562838_n.jpg';
 export default function App() {
+  const canal=()=>{
+    return("Varnahal")
+  }
+  const curso=()=>{
+    return("Curso de react")
+  }
+
   return(
     <>
     <section>
-      <p>Curso</p>
+    <header>
+      <p>Canal: {"de games->"+canal()}</p>
       <br/>
-      <p>Curso de react</p>
+      <p>{curso()}</p>
+    </header>
+      <div>
+        meu pao
+      </div>
+      <h1>José</h1>
+      <img src={Logo}/>
+      <h1>Penio</h1>
+      <img src="/imgs/tenor.gif"/>
     </section>
     </>
   );
