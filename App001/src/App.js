0dd2fe98269a30299089cdb59a3fbc400d9f32fa
Varnahal <danielmarcelino91@gmail.com>
2022-11-15 15:38:45 -0300

    manipulando valores de formulario

--- App001/src/App.js
+++ App001/src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
-import Topo from './componentes/Topo.js';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [nome,setnome] = useState('');
+  const [carro,setcarro] = useState('HRV');
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <Topo/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <label>Digite seu nome: </label>
+        <input type='text' name='fnome' onChange={(e)=>setnome(e.target.value)}></input>
+        <p>Nome digitado:{nome}</p>
+        <label>Selecione um carro</label>
+        <select value={carro} onChange={(e)=>{setcarro(e.target.value)}}>
+          <option value='HRV'>HRV</option>
+          <option value='Golf'>Golf</option>
+          <option value='Cruze'>Cruze</option>
+          <option value='Argo'>Argo</option>
+        </select>
+        <p>Carro selecionado: {carro}</p>
+      </form>
+    </>
   );
 }
 
