e478b7210f22ac66520644cb12904365ad0d0a8a
Varnahal <danielmarcelino91@gmail.com>
2022-11-17 20:39:33 -0300

    terminei aula de passagem de objetos no state

--- App001/src/App.js
+++ App001/src/App.js
@@ -3,25 +3,11 @@ import './App.css';
 
 function App() {
 
-  const [nome,setnome] = useState('');
-  const [carro,setcarro] = useState('HRV');
 
   
   return (
     <>
-      <form>
-        <label>Digite seu nome: </label>
-        <input type='text' name='fnome' onChange={(e)=>setnome(e.target.value)}></input>
-        <p>Nome digitado:{nome}</p>
-        <label>Selecione um carro</label>
-        <select value={carro} onChange={(e)=>{setcarro(e.target.value)}}>
-          <option value='HRV'>HRV</option>
-          <option value='Golf'>Golf</option>
-          <option value='Cruze'>Cruze</option>
-          <option value='Argo'>Argo</option>
-        </select>
-        <p>Carro selecionado: {carro}</p>
-      </form>
+
     </>
   );
 }
