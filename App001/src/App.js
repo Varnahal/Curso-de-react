03a399bb77401a7ca3550cd121049f4a05070ca0
Varnahal <danielmarcelino91@gmail.com>
2022-12-01 18:10:29 -0300

    categoria de carros em react

--- App001/src/App.js
+++ App001/src/App.js
@@ -1,36 +1,68 @@
 import React,{useState,useEffect} from 'react';
-import Pagina1 from './componentes/Pagina1.js';
-import Pagina2 from './componentes/Pagina2.js';
 import './App.css';
 
+  const carros= [
+    {categoria:"esporte",preco:"110000",modelo:"Golf Gti"},
+    {categoria:"esporte",preco:"120000",modelo:"camaro"},
+    {categoria:"SUV",preco:"85000",modelo:"HRV"},
+    {categoria:"SUV",preco:"83000",modelo:"T-Cross"},
+    {categoria:"Ultilitario",preco:"125000",modelo:"Hillux"},
+    {categoria:"Ultilitario",preco:"90000",modelo:"Ranger"},
+
+  ]
+  const linhas=(cat)=>{
+    const li=[]
+    carros.forEach(
+      (carro)=>{
+        if(carro.categoria.toUpperCase()==cat.toUpperCase()||cat==''){
+          li.push(
+            <tr>
+              <td>{carro.categoria}</td>
+              <td>{carro.preco}</td>
+              <td>{carro.modelo}</td>
+            </tr>
+          )
+        }
+      }
+    )
+    return li
+  }
+
+  const tabelacarros=(cat)=>{
+    return(
+      <table border= '1' style={{borderCollapse:'colapse'}}>
+        <thead>
+          <tr>
+            <th>Categoria</th><th>Preço</th><th>Modelo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {linhas(cat)}
+        </tbody>
+      </table>
+    )
+
+  }
+  const pesquisacat=(cat,scat)=>{
+    return(
+      <div>
+        <label>Digite uma categoria</label>
+        <input type='text' value={cat} onChange={(e)=>{scat(e.target.value)}}></input>
+      </div>
+    )
+
+  }
+
 function App() {
-
-  const[pagina,setpagina] = useState(0)
-
-  const linksPaginas = (p)=>{
-    setpagina(p)
-  }
+  const [categoria,setcategoria] = useState('');
 
 
-  const retornarpagina = ()=>{
-    if(pagina==1){
-      return <Pagina1/>
-    }else if(pagina==2){
-      return <Pagina2/>
-    }else{
-      return  <div>
-                <button onClick={()=>{setpagina(1)}}>Pagina1</button>
-                <button onClick={()=>{setpagina(2)}}>Pagina2</button>
-              </div>
-    
-    }
-
-  }
 
   return (
     
       <>
-      {retornarpagina()}
+      {pesquisacat(categoria,setcategoria)}
+        {tabelacarros(categoria)}
       
       </>
       
