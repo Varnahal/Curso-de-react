8d1794450d62662801a1cebf1b144f414ea7714c
Varnahal <danielmarcelino91@gmail.com>
2022-12-08 19:07:52 -0300

    começando a calculadora em react

--- App001/src/App.js
+++ App001/src/App.js
@@ -1,26 +1,90 @@
-import React,{useState,useEffect} from 'react';
-import TabelaImc from './componentes/Ctabelaimc.js';
-import Fpeso from './componentes/Cpeso.js';
-import Faltura from './componentes/Caltura.js';
-import Fcalcular from './componentes/Ccalcular.js';
-import Resultado from './componentes/Cresultado.js'
+import React,{useState} from 'react';
 import './App.css';
 
 
 
 export default function App(){
-  const[peso,setPeso] = useState(0);
-  const[altura,setAltura] = useState(0);
-  const[resultado,setResultado] = useState(0);
+
+  const [valorTela, setvalorTela] = useState('')
+  const [resultado,setresultado] = useState(0)
+  const [acumulador, setacumulador] = useState(0)
+  const [operado,setoperado] = useState(false)
+
+  //funções
+
+  const addDigitoTela=(d)=>{
+    if(((d=='+'||d=='-'||d=='*'||d=='/')&&operado)){
+      console.log('+-*/')
+      setoperado(false)
+      setvalorTela(resultado+d)
+      return
+    }
+    if(operado){
+      setvalorTela(d)
+      setoperado(false)
+      return
+    }
+    const valorDigitadoNaTela=valorTela+d
+    setvalorTela(valorDigitadoNaTela)
+  }
+  const limparMemoria=()=>{
+    setoperado(false)
+    setvalorTela('')
+    setresultado(0)
+    setacumulador(0)
+    return
+  }
+
+  //componentes
+  const Tela=(valor,res)=>{
+    return(
+      <div style = {csstela}>
+        <span style={csstelaRes}>{valor}</span>
+        <span style={csstelaRes}>{res}</span>
+      </div>
+    )
+  }
+  const Btn=(label,oC)=>{
+    return (
+      <button style={cssBtn} onClick={oC}>{label}</button>
+    )
+  }
+  //estilos
+  const csstela={
+    display:'flex',
+    paddingLeft:20,
+    paddingRight:20,
+    justifyContent:'center',
+    alignItens:'flex-start',
+    backgroundColor:'#444',
+    flexDirection:'column',
+    window:260,
+  }
+  const csstelaOper={
+    fontSize:25,
+    color:'#fff',
+    height:20,
+  }
+  const csstelaRes={
+    fontSize:50,
+    color:'#fff',
+  }
+  const cssBtn={
+    fontSize:30,
+    height:75,
+    width:75,
+    paddig:20,
+    backgroundColor:'#000',
+    color:'#fff',
+    borderColor:'#000',
+    textAlign:'center',
+    outline:'none',
+  }
 
   return (
     
       <>
-        <Fpeso p={peso} sp={setPeso}/>
-        <Faltura a={altura} sa={setAltura}/> 
-        <Fcalcular p={peso} a={altura} sr={setResultado}/>
-        <Resultado res={resultado}/>
-        <TabelaImc/>
+        
       </>
       
     
