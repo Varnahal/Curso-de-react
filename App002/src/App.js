3ccb0998669fcb5972372db17e49ef7aed6f4be7
Varnahal <danielmarcelino91@gmail.com>
2022-12-12 20:39:00 -0300

    inicio e meio jogo da velha

--- App002/src/App.js
+++ App002/src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
+import react,{useState} from 'react'
 import './App.css';
 
-function App() {
+export default function App() {
+
+  //Estilos
+
+  const tabu ={
+    fisplay:'flex',
+    flexDirection:'row'
+  }
+
+  const tabuLinha = {
+    display:'flex',
+    flexDirection:'row'
+  }
+  const casa ={
+    width:100,
+    height:100,
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'row',
+    cursor:'pointer',
+    fontSize:60,
+    border:'1px solid #000'
+
+  }
+  const jogoInicial = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ]
+  const [jogo,setjogo] = useState(jogoInicial)
+  const [simboloatual,setsimboloatual] = useState('X')
+  const [jogando,setjogando]=useState(true)
+
+  const tabuleiro=(j)=>{
+    return(
+      <div style={tabu}>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='00' onClick={''}>{j[0][0]}</div>
+          <div style={casa} data-pos='01' onClick={''}>{j[0][1]}</div>
+          <div style={casa} data-pos='02' onClick={''}>{j[0][2]}</div>
+        </div>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='10' onClick={''}>{j[1][0]}</div>
+          <div style={casa} data-pos='11' onClick={''}>{j[1][1]}</div>
+          <div style={casa} data-pos='12' onClick={''}>{j[1][2]}</div>
+        </div>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='20' onClick={''}>{j[2][0]}</div>
+          <div style={casa} data-pos='21' onClick={''}>{j[2][1]}</div>
+          <div style={casa} data-pos='22' onClick={''}>{j[2][2]}</div>
+        </div>
+      </div>
+    )
+  }
+
+  const verificaVitoria=()=>{
+    //linhas
+    let pontos=0
+    let vitoria=false
+    for (let l = 0; l < 3; l++) {
+      pontos=0
+      for (let c = 0; c < 3; c++) {
+        if(jogo[l][c]==simboloatual){
+          pontos++
+        }
+        
+      }
+      if(pontos>=3){
+        vitoria=true
+        break
+      }
+    }
+    //colunas
+    for (let c = 0; c < 3; c++) {
+      pontos=0
+      for (let l = 0; l < 3; l++) {
+        if(jogo[l][c]==simboloatual){
+          pontos++
+        }
+        
+      }
+      if(pontos>=3){
+        vitoria=true
+        break
+      }
+      
+    }
+
+    //diagonais
+    pontos=0
+    for (let d = 0; d < 3; d++) {
+      if(jogo[d][d]){
+        if(jogo[d][d]==simboloatual){
+          pontos++
+        }
+      }
+    }
+    if(pontos>=3){
+      vitoria=true
+      
+    }
+    pontos=0
+    let l=0
+    for (let c = 2; c >=0; c--) {
+      if(jogo[l][c]==simboloatual){
+        pontos++
+      }
+      l++
+    }
+    if(pontos>=3){
+      vitoria=true
+      
+    }
+    return vitoria
+
+  }
+  const trocaJogador=()=>{
+    simboloatual=='X'?setsimboloatual('O'):setsimboloatual('X')
+  }
+  const retPos=(e)=>{
+    const p = e.target.getAttribute('data-pos')
+    const pos = [parseInt(p.substring(0,1)),parseInt(p.substring(1,2))]
+    return pos
+  }
+  const verificaespacovazio=(e)=>{
+    if(jogo[retPos(e)[0]][retPos(e)[1]]==''){
+      return true
+    }else{
+      return false
+    }
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {tabuleiro(jogoInicial)}
+
+    </>
   );
 }
 
-export default App;
