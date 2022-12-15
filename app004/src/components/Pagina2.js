c6e5b74d01747c487fc7dde55488482dfae1ed67
Varnahal <danielmarcelino91@gmail.com>
2022-12-15 18:11:26 -0300

    curso de react finalizado

---
+++ app004/src/components/Pagina2.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+export default function Pagina2(){
+    return(
+        <div>
+            <h1>Página 2</h1>
+            <h3>Varnahal</h3>
+        </div>
+    )
+}
---
+++ app004/src/components/Pagina3.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+export default function Pagina3(){
+    return(
+        <div>
+            <h1>Página 3</h1>
+            <h3>Usando rotas</h3>
+        </div>
+    )
+}
